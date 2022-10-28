41ca838302491da08739a2f4a537112005c3d33b
erickgr121 <104312513+erickgr121@users.noreply.github.com>
2022-10-28 11:44:24 -0300

    Add files via upload

---
+++ a.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="a.css">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <div class=post-php>
+  <?php
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+
+
+    ?>
+  </div>
+  <div class="boas-vindas">
+
+
+    <h1>Seja bem vindo (a), Aluno</h1> <br>
+
+
+
+  </div>
